726454e14e934d770b01118cc5f3ece41ad750df
bellero-phon <51389521+bellero-phon@users.noreply.github.com>
2019-08-05 21:16:36 +0000

    added loop

--- cl.py
+++ cl.py
@@ -1,15 +1,24 @@
 import pickle
 import vectorize
+import os
 
+os.system('cls' if os.name == 'nt' else 'clear')
 f = open('./vectors.pkl', 'rb')
 dict = pickle.load(f)
 f.close()
 
 print('Word Unscrambler')
-word = input('Enter a scrambled word: ')
-key = vectorize.get_key(word)
 
-if key in dict.keys():
-    print(''.join(dict[key])[:-1])
-else:
-    print('Not found')
+def coded_letters():
+    word=''
+    while word.lower().startswith('q')!=True:
+        word=input('Enter a scrambled word or \"\'q\'uit\": ')
+        key=vectorize.get_key(word)
+        if word.lower().startswith('q')==True:
+            break
+        elif key in dict.keys():
+            print(''.join(dict[key])[:-1])
+        else:
+            print('Not found')
+
+coded_letters()
